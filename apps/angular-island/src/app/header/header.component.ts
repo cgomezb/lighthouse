912284be870e68644094a149e426905664427fc2
Cesar Gomez <cesargomez290@gmail.com>
2021-11-05 23:29:29 -0500

    feat: CG-0002 Updated header component

--- apps/angular-island/src/app/header/header.component.ts
+++ apps/angular-island/src/app/header/header.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'lighthouse-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HeaderComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class HeaderComponent {}
