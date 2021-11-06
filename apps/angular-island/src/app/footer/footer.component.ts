ed940603a65e2bfd6ae0466b9b759be4de5ac49d
Cesar Gomez <cesargomez290@gmail.com>
2021-11-05 23:31:40 -0500

    feat: CG-0002 Updated footer component

--- apps/angular-island/src/app/footer/footer.component.ts
+++ apps/angular-island/src/app/footer/footer.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'lighthouse-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FooterComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class FooterComponent {}
