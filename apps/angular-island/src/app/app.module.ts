d9ae7b8d7913ae754480906210073f04daa1f3f2
Cesar Gomez <cesargomez290@gmail.com>
2021-11-05 23:30:39 -0500

    feat: CG-0002 Added footer component into angular island project

--- apps/angular-island/src/app/app.module.ts
+++ apps/angular-island/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
