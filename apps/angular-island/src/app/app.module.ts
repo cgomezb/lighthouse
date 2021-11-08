0a55c1816da66c88a2178eccf146507a8e88e706
Cesar Gomez <cesargomez280@gmail.com>
2021-11-07 19:40:24 -0500

    feat: CG-0002 Added container component into module

--- apps/angular-island/src/app/app.module.ts
+++ apps/angular-island/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, ContainerComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
