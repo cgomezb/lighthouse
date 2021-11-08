be5ea1cb3bd3f5d4d32392bbba84994949408c38
Cesar Gomez <cesargomez280@gmail.com>
2021-11-07 19:41:22 -0500

    feat: CG-0002 Updated container component

--- apps/angular-island/src/app/container/container.component.ts
+++ apps/angular-island/src/app/container/container.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'lighthouse-container',
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.scss']
 })
-export class ContainerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class ContainerComponent {}
