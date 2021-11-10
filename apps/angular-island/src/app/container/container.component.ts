5918cc5ddb648107c61396e8d45a6aa115ae7653
Cesar Gomez <cesargomez280@gmail.com>
2021-11-09 22:22:27 -0500

    feat: CG-0002 Updated container component input

--- apps/angular-island/src/app/container/container.component.ts
+++ apps/angular-island/src/app/container/container.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'lighthouse-container',
@@ -7,4 +7,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class ContainerComponent {}
+export class ContainerComponent {
+  @Input() content = '';
+}
